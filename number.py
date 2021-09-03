e5a5616cd1691e64ecebdf86cbda9dd3a7034b1d
Harathi <ravinuthalaharathi@gmail.com>
2021-09-03 11:26:59 +0530

    number commit

---
+++ number.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the number"))
+if n==1:
+    print("one")
+elif n==2:
+    print("Two")
+elif n==3:
+    print("Three")
+elif n==4:
+    print("four")
+elif n==5:
+    print("six")
+else:
+    print("invalid number")
