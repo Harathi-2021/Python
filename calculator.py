1abc28d2175a64be215cfb18619a7d48ee31e225
Harathi <ravinuthalaharathi@gmail.com>
2021-09-03 12:20:08 +0530

    calculate commit

--- calculator.py
+++ calculator.py
@@ -1,6 +1,17 @@
 x=int(input("Enter the number"))
-y=int(input("Enter tne number")
-if x+y==0:
-    print("two numbers")
+y=int(input("Enter the number"))
+if x+y==34:
+    print(f" In calculator subtraction of two numbers={x-y}")
+elif x*y==40:
+    print(f" In calculator multiplication of two numbers ={x*y}")
+elif x/y==10:
+    print(f"In calculator division of two numbers={x/y}")
+elif x%y==20:
+    print(f"calculate addition of two numbers={x+y}")
+elif x%y==20:
+    print(f"In calculator modulo of two numbers={x%y}")
+elif x//y==74:
+    print(f"In calculator division of two numbers={x//y}")
 else:
-    print("sub")
+    print("no numbers to calculate")
+
