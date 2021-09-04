9dc5984cd72672eb2d32a8b03f988d81c55c8aea
Harathi <ravinuthalaharathi@gmail.com>
2021-09-04 13:02:16 +0530

    calculator commit

--- calculator.py
+++ calculator.py
@@ -1,19 +1,26 @@
-x=int(input("Enter the number"))
-y=int(input("Enter the number"))
-if x+y==34:
-    print(f" In calculator addition of two numbers={x+y}")
-elif x-y==0:
-    print(f"In subtraction of two numbers ={x-y}")
-elif x*y==40:
-    print(f" In calculator multiplication of two numbers ={x*y}")
-elif x/y==10:
-    print(f"In calculator division of two numbers={x/y}")
-elif x%y==20:
-    print(f"calculate addition of two numbers={x+y}")
-elif x%y==20:
-    print(f"In calculator modulo of two numbers={x%y}")
-elif x//y==74:
-    print(f"In calculator division of two numbers={x//y}")
+choice=int(input("Enter the choice number"))
+if choice==1:
+    a=int(input("Enter the number"))
+    b=iint(input("Enter the number"))
+    c=a+b
+    print("Result=",c)
+elif choice==2:
+    a=int(input("Enter the number"))
+    b=int(input("Enter the number"))
+    c=a-b
+    print("Result=",c)
+elif choice==3:
+    a=int(input("Enter the number"))
+    b=int(input("Enter the number"))
+    c=a*b
+    print("Result=",c)
+elif choice==4:
+    a=int(input("Enter the number"))
+    b=int(input("Enter the number"))
+    c=a/b
+    print("Result=",c)
+elif choice==5:
+    exit()
 else:
-    print("no numbers to calculate")
+    print("wrong number")
 
