4540873ea123bba45044b55e2cf1f2cadc0d6136
Harathi <ravinuthalaharathi@gmail.com>
2021-09-06 17:49:50 +0530

    group commit

--- calculator.py
+++ calculator.py
@@ -1,23 +1,16 @@
 choice=int(input("Enter the choice number 1:addtion 2:subtraction 3:multiplication 4:division"))
-
+a=int(input("Enter the number"))
+b=int(input("enter the number"))
 if choice==1:
-    a=int(input("Enter the number"))
-    b=int(input("Enter the number"))
     c=a+b
     print("Result=",c)
 elif choice==2:
-    a=int(input("Enter the number"))
-    b=int(input("Enter the number"))
     c=a-b
     print("Result=",c)
 elif choice==3:
-    a=int(input("Enter the number"))
-    b=int(input("Enter the number"))
     c=a*b
     print("Result=",c)
 elif choice==4:
-    a=int(input("Enter the number"))
-    b=int(input("Enter the number"))
     c=a/b
     print("Result=",c)
 elif choice==5:
