b45de334f126b5bd501a48fb796d148ba1c41865
Harathi <ravinuthalaharathi@gmail.com>
2021-09-07 12:09:56 +0530

    ag groups

---
+++ ag groups.py
@@ -0,0 +1,15 @@
+age=int(input("Enter the age"))
+if age>=0 and age<=4:
+    print("Kids")
+elif age>=5 and age<=9:
+    print("child")
+elif age>=10 and age<=17:
+    print("Adult")
+elif age>=18 and age<=21:
+    print("mature")
+elif age>=22 and age<=24:
+    print("people")
+elif age>=25 and age<=25:
+    print("people")
+else:
+    print("invalid number")
--- amount.py
+++
@@ -1,5 +0,0 @@
-amount=int(input("Enter the amount"))
-if (amount/100):
-    print("1%")
-else:
-    print(" 0% numbber")
